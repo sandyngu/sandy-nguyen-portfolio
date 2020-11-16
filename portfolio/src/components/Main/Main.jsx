fb57ad96e1b792344ee26c96503642ae63d8d824
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-11-16 15:27:34 -0500

    Projects Component Initiated

--- portfolio/src/components/Main/Main.jsx
+++ portfolio/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
+import Projects from '../Projects/Projects';
 
 function Main() {
     return (
         <div className="main">
             <Header />
             <Hero />
+            <Projects />
         </div>
     )
 }
---
+++ portfolio/src/components/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './projects.scss';
+
+function Projects() {
+    return (
+        <div className="projects">
+            <div className="projects__container">
+                <p className="projects__title">projects</p>
+                <div className="projects__line"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
