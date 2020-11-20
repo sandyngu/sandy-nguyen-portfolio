08e12d26f498a0bd06635c646356f3056403ab61
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-11-19 21:45:39 -0500

    Header Arrow Animations Styled

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -3,13 +3,54 @@ import { Link } from 'react-router-dom';
 import './header.scss';
 
 function Header() {
+
+    function mouseHover(e) {
+        console.log(e.target.className)
+        document.querySelector('.header__nav-list-item--left1').classList.add('header__nav-list-item--hover1');
+        document.querySelector('.header__nav-list-item--right1').classList.add('header__nav-list-item--hover2');
+        document.querySelector('.header__nav-list-item--left2').classList.add('header__nav-list-item--hover3');
+        document.querySelector('.header__nav-list-item--right2').classList.add('header__nav-list-item--hover4');
+        document.querySelector('.header__nav-list-item--left3').classList.add('header__nav-list-item--hover5');
+        document.querySelector('.header__nav-list-item--right3').classList.add('header__nav-list-item--hover6');
+    }
+
+    function mouseLeave() {
+        document.querySelector('.header__nav-list-item--left1').classList.remove('header__nav-list-item--hover1');
+        document.querySelector('.header__nav-list-item--right1').classList.remove('header__nav-list-item--hover2');
+        document.querySelector('.header__nav-list-item--left2').classList.remove('header__nav-list-item--hover3');
+        document.querySelector('.header__nav-list-item--right2').classList.remove('header__nav-list-item--hover4');
+        document.querySelector('.header__nav-list-item--left3').classList.remove('header__nav-list-item--hover5');
+        document.querySelector('.header__nav-list-item--right3').classList.remove('header__nav-list-item--hover6');
+    }
+
     return (
         <div className="header">
             <nav className="header__nav">
                 <ul className="header__nav-list">
-                    <Link className="header__nav-link"><li className="header__nav-list-item"><span className="header__nav-list-item--hover">&lt;</span> about me /<span className="header__nav-list-item--hover">&gt;</span></li></Link>
-                    <Link className="header__nav-link"><li className="header__nav-list-item"><span className="header__nav-list-item--hover">&lt;</span> projects /<span className="header__nav-list-item--hover">&gt;</span></li></Link>
-                    <Link className="header__nav-link"><li className="header__nav-list-item"><span className="header__nav-list-item--hover">&lt;</span> contact /<span className="header__nav-list-item--hover">&gt;</span></li></Link>
+                    <Link className="header__nav-link">
+                        <li className="header__nav-list-item header__nav-list-item1" onMouseOver={(e) => mouseHover(e)} onMouseLeave={() => mouseLeave()}>
+                            <div className="header__nav-list-item--left1">&lt; </div> about me 
+                            <span className="header__nav-list-item-line1">/</span>
+                            <span className="header__nav-list-item-line2">/</span>
+                            <div className="header__nav-list-item--right1">&gt;</div>
+                        </li>
+                    </Link>
+                    <Link className="header__nav-link">
+                        <li className="header__nav-list-item header__nav-list-item2" onMouseOver={(e) => mouseHover(e)} onMouseLeave={() => mouseLeave()}>
+                            <div className="header__nav-list-item--left2">&lt; </div> projects 
+                            <span className="header__nav-list-item-line3">/</span>
+                            <span className="header__nav-list-item-line4">/</span>
+                            <div className="header__nav-list-item--right2">&gt;</div>
+                        </li>
+                    </Link>
+                    <Link className="header__nav-link">
+                        <li className="header__nav-list-item header__nav-list-item" onMouseOver={(e) => mouseHover(e)} onMouseLeave={() => mouseLeave()}>
+                            <div className="header__nav-list-item--left3">&lt; </div> contact 
+                            <span className="header__nav-list-item-line5">/</span>
+                            <span className="header__nav-list-item-line6">/</span>
+                            <div className="header__nav-list-item--right3">&gt;</div>
+                        </li>
+                    </Link>
                 </ul>
             </nav>
         </div>
